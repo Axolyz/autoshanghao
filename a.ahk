795c3869699d162f748f5323c78e844ae5d71e03
Li Baoyan <792405142@qq.com>
2020-04-13 13:00:44 +0800

    init

---
+++ a.ahk
@@ -0,0 +1,15 @@
+﻿SetWorkingDir,%A_ScriptFullPath%
+Run,云视讯
+WinActivate, ViLin ahk_class YSXForm
+Sleep,8000
+ImageSearch, OutputVarX, OutputVarY,0,0,500,500,*50 2.bmp
+Click %OutputVarX% %OutputVarY%
+Sleep,1000
+ImageSearch, OutputVarX, OutputVarY,0,0,500,500,*50 1.bmp
+Click %OutputVarX% %OutputVarY%
+Send,{Tab}
+Send, {LControl Down}
+Send, {v}
+Send, {LControl Up}
+Send,{Enter}
+Return
---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+@Author       : Li Baoyan
+@Date         : 2020-04-01 12:20:26
+@Github       : https://github.com/This-username-is-available
+@Description  : ?
+"""
+
+import xlrd, datetime, pyperclip, os, json
+import os, sys
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+with open("作息时间.json", "r", encoding="utf8") as idiom_file:
+    schedule = json.load(idiom_file)
+schedule = [x[0] * 60 + x[1] + 40 for x in schedule]
+
+xingqi = datetime.datetime.strftime(datetime.datetime.now(), "%w")
+h = datetime.datetime.strftime(datetime.datetime.now(), "%H")
+m = datetime.datetime.strftime(datetime.datetime.now(), "%M")
+shijian = int(h) * 60 + int(m)
+
+book = xlrd.open_workbook("号号.xlsx")
+
+sheet1 = book.sheets()[0]
+
+
+for x in schedule:
+    if shijian < x:
+        cell1 = sheet1.cell(int(schedule.index(x)), int(xingqi)).value
+        print(str(cell1))
+        break
+a = str(int(cell1))
+pyperclip.copy(a)  # 向剪切板发送文本
+os.system("a.ahk")
